fb222b86fa13d6c37d2ed622e9e0c174ee9b88ab
Spayker <spykerstar@gmail.com>
2018-02-04 23:52:15 +0200

    - Added Settings and main menu activities; - Added interfaces for further service layer implementation;

---
+++ BridgeCom/app/src/main/java/com/spand/bridgecom/service/channel/ChannelProvider.java
@@ -0,0 +1,8 @@
+package com.spand.bridgecom.service.channel;
+
+/**
+ * Created by Spayker on 2/4/2018.
+ */
+
+public interface ChannelProvider {
+}
---
+++ BridgeCom/app/src/main/java/com/spand/bridgecom/service/user/common/validation/Validator.java
@@ -0,0 +1,8 @@
+package com.spand.bridgecom.service.user.common.validation;
+
+/**
+ * Created by Spayker on 2/4/2018.
+ */
+
+public interface Validator {
+}
