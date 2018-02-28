8e8947b1080f836ee5a1fff00004b7e6f5e0ada2
Spayker <spykerstar@gmail.com>
2018-02-28 21:26:02 +0200

    - Updated activity list;

--- BridgeCom/app/src/main/java/com/spand/bridgecom/activity/ChannelsSettingActivity.java
+++ BridgeCom/app/src/main/java/com/spand/bridgecom/activity/ChannelsSettingActivity.java
@@ -1,7 +1,9 @@
 package com.spand.bridgecom.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.spand.bridgecom.R;
 
@@ -11,5 +13,10 @@ public class ChannelsSettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channels_setting);
+    }
+
+    public void selectChannelSetting(View view) {
+        Intent intent = new Intent(this, CertainChannelSettingActivity.class);
+        startActivity(intent);
     }
 }
--- BridgeCom/app/src/main/java/com/spand/bridgecom/activity/GlobalSettingsActivity.java
+++ BridgeCom/app/src/main/java/com/spand/bridgecom/activity/GlobalSettingsActivity.java
@@ -1,20 +1,33 @@
 package com.spand.bridgecom.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.spand.bridgecom.R;
 
 public class GlobalSettingsActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_global_settings);
     }
 
+    public void selectChangePassword(View view) {
+        view.getTransitionName();
+        Intent intent = new Intent(this, ChangePasswordActivity.class);
+        startActivity(intent);
+    }
 
+    public void selectResetSettings(View view) {
+        Intent intent = new Intent(this, ResetSettingsActivity.class);
+        startActivity(intent);
+    }
 
-
+    public void selectDeactivateAccount(View view) {
+        Intent intent = new Intent(this, RemoveAccountActivity.class);
+        startActivity(intent);
+    }
 
 }
