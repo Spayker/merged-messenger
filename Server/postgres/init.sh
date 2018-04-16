a881e2a4ea93bb29b63b45dfadb13d0155c9c28c
Spayker <spykerstar@gmail.com>
2018-04-16 19:17:29 +0200

    - Updated production db name in config files of docker configuration;

--- Server/postgres/init.sh
+++ Server/postgres/init.sh
@@ -9,10 +9,10 @@ set -o errexit
 # that must be set. This can be consumed later via arrray
 # variable expansion ${REQUIRED_ENV_VARS[@]}.
 readonly REQUIRED_ENV_VARS=(
-  "TEST_DB_USER"
-  "TEST_DB_PASSWORD"
-  "TEST_DB_NAME"
-  "TEST_DB_SCHEME"
+  "BRIDGECOM_DB_USER"
+  "BRIDGECOM_DB_PASSWORD"
+  "BRIDGECOM_DB_NAME"
+  "BRIDGECOM_DB_SCHEME"
   "POSTGRES_PASSWORD")
 
 
@@ -26,14 +26,14 @@ main() {
 # Performs the initialization in the already-started PostgreSQL
 init_user_and_db() {
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-     CREATE USER $TEST_DB_USER WITH PASSWORD '$TEST_DB_PASSWORD';
-     CREATE DATABASE $TEST_DB_NAME
-       WITH OWNER = $TEST_DB_USER
+     CREATE USER $BRIDGECOM_DB_USER WITH PASSWORD '$BRIDGECOM_DB_PASSWORD';
+     CREATE DATABASE $BRIDGECOM_DB_NAME
+       WITH OWNER = $BRIDGECOM_DB_USER
             ENCODING = 'UTF8'
             TABLESPACE = pg_default
             CONNECTION LIMIT = -1;
-            \connect $TEST_DB_NAME;
-     CREATE SCHEMA $TEST_DB_SCHEME AUTHORIZATION $TEST_DB_USER;
+            \connect $BRIDGECOM_DB_NAME;
+     CREATE SCHEMA $BRIDGECOM_DB_SCHEME AUTHORIZATION $BRIDGECOM_DB_USER;
 EOSQL
 }
 main "$@"
