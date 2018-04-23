8726883eee7172edb1c261aabf32cac8f77096a4
Oleksandr Kuzmin <oleksandr_kuzmin@epam.com>
2018-04-23 11:36:56 +0200

    - First init commit for fb module;

---
+++ BridgeCom/fb/src/main/java/com/spand/bridgecom/fb/common/authorization/Login.java
@@ -0,0 +1,24 @@
+package com.spand.bridgecom.fb.common.authorization;
+
+import com.facebook.CallbackManager;
+
+public class Login {
+
+    private CallbackManager callbackManager = CallbackManager.Factory.create();
+
+    public Login(CallbackManager callbackManager){
+        this.callbackManager = callbackManager;
+    }
+
+    public void performLogin(){
+        // toDo: implement login logic below
+    }
+
+
+
+
+
+
+
+
+}
---
+++ BridgeCom/fb/src/main/java/com/spand/bridgecom/fb/common/authorization/Logout.java
@@ -0,0 +1,14 @@
+package com.spand.bridgecom.fb.common.authorization;
+
+public class Logout {
+
+
+    public Logout(){
+        // toDo: implement constructor logic below if it's needed
+    }
+
+    public void performLogout()  {
+        // toDo: implement logout logic below
+    }
+
+}
