32687b82f7de2b0ec5e3118b33dd4d796e3d9b1e
Spayker <spykerstar@gmail.com>
2018-11-05 17:00:40 +0100

    - Updated user authorize test base;

--- MeMe/app/src/test/java/com/spandr/meme/core/activity/intro/logic/data/UserTest.java
+++ MeMe/app/src/test/java/com/spandr/meme/core/activity/intro/logic/data/UserTest.java
@@ -1,6 +1,5 @@
 package com.spandr.meme.core.activity.intro.logic.data;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 
 import com.spandr.meme.core.activity.authorization.LoginActivity;
@@ -11,9 +10,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertTrue;
 import static junit.framework.TestCase.fail;
 
 @RunWith(RobolectricTestRunner.class)
@@ -25,6 +25,7 @@ public class UserTest {
         AppCompatActivity appCompatActivity = null;
         // when
         User.getInstance(appCompatActivity);
+        // then
         fail("app compat activity: object is null");
     }
 
@@ -35,6 +36,26 @@ public class UserTest {
 
         // when
         User user = User.getInstance(activity);
+        // then
         assertNotNull(user);
     }
+
+    @Test
+    public void getUserInstanceWithEmptyFieldsTest(){
+        // given
+        AppCompatActivity activity = Robolectric.setupActivity(LoginActivity.class);
+
+        // when
+        User user = User.getInstance(activity);
+        // then
+        assertTrue(user != null);
+        assertTrue(user.getUserName() != null);
+        assertTrue(user.getEmailAddress() != null);
+        assertTrue(user.getPassword() != null);
+        assertTrue(user.getUserName().isEmpty());
+        assertTrue(user.getEmailAddress().isEmpty());
+        assertTrue(user.getPassword().isEmpty());
+    }
+
+
 }
