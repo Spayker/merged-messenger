54dd6b847ac995c5f29ecd4560b07ab314f0ea2b
Spayker <spykerstar@gmail.com>
2018-11-05 17:40:17 +0100

    - Updated user authorize test base;

--- MeMe/app/src/test/java/com/spandr/meme/core/activity/intro/logic/data/UserTest.java
+++ MeMe/app/src/test/java/com/spandr/meme/core/activity/intro/logic/data/UserTest.java
@@ -1,5 +1,7 @@
 package com.spandr.meme.core.activity.intro.logic.data;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 
 import com.spandr.meme.core.activity.authorization.LoginActivity;
@@ -11,6 +13,10 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 
+import static com.spandr.meme.core.activity.main.logic.starter.SettingsConstants.KEY_PASS;
+import static com.spandr.meme.core.activity.main.logic.starter.SettingsConstants.KEY_USER_EMAIL_OR_PHONE;
+import static com.spandr.meme.core.activity.main.logic.starter.SettingsConstants.KEY_USER_NAME;
+import static com.spandr.meme.core.activity.main.logic.starter.SettingsConstants.PREF_NAME;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
@@ -55,6 +61,35 @@ public class UserTest {
         assertTrue(user.getUserName().isEmpty());
         assertTrue(user.getEmailAddress().isEmpty());
         assertTrue(user.getPassword().isEmpty());
+    }
+
+    @Test
+    public void getUserInstanceWithFieldsTest(){
+        // given
+        AppCompatActivity activity = Robolectric.setupActivity(LoginActivity.class);
+        SharedPreferences sharedPreferences = activity.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String userName = "alex";
+        String email    = "alex@gmail.com";
+        String password = "qwerty";
+
+        editor.putString(KEY_USER_NAME, userName);
+        editor.putString(KEY_USER_EMAIL_OR_PHONE, email);
+        editor.putString(KEY_PASS, password);
+        editor.commit();
+
+        // when
+        User user = User.getInstance(activity);
+
+        // then
+        assertTrue(user != null);
+        assertTrue(user.getUserName() != null);
+        assertTrue(user.getEmailAddress() != null);
+        assertTrue(user.getPassword() != null);
+
+        assertEquals(user.getUserName(), userName);
+        assertEquals(user.getEmailAddress(), email);
+        assertEquals(user.getPassword(), password);
     }
 
 
