8ec16541f8b3e5dd3fc22dd2feaafaabe17eddbc
Spayker <spykerstar@gmail.com>
2018-11-05 16:44:53 +0100

    - Added robolectric dependency to test in full emulated app activities; - Minor changes in test base;

--- MeMe/app/src/test/java/com/spandr/meme/core/activity/intro/logic/data/UserTest.java
+++ MeMe/app/src/test/java/com/spandr/meme/core/activity/intro/logic/data/UserTest.java
@@ -1,21 +1,26 @@
 package com.spandr.meme.core.activity.intro.logic.data;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 
+import com.spandr.meme.core.activity.authorization.LoginActivity;
 import com.spandr.meme.core.activity.authorization.logic.data.User;
 import com.spandr.meme.core.activity.authorization.logic.exception.AppAuthorizationException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+import static junit.framework.Assert.assertNotNull;
 import static junit.framework.TestCase.fail;
 
-@RunWith(JUnit4.class)
+@RunWith(RobolectricTestRunner.class)
 public class UserTest {
 
     @Test(expected = AppAuthorizationException.class)
-    public void getInstanceWithNullActivityAndAppAuthorizationException() {
+    public void getUserInstanceWithNullActivityAndAppAuthorizationException() {
         // given
         AppCompatActivity appCompatActivity = null;
         // when
@@ -23,7 +28,13 @@ public class UserTest {
         fail("app compat activity: object is null");
     }
 
+    @Test
+    public void getUserInstanceWithNonNullActivityTest() {
+        // given
+        AppCompatActivity activity = Robolectric.setupActivity(LoginActivity.class);
 
-
-
+        // when
+        User user = User.getInstance(activity);
+        assertNotNull(user);
+    }
 }
