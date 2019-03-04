76e7abb285b998d825b80528e7eafbdcc9473f4f
Spayker <spykerstar@gmail.com>
2019-03-04 12:19:52 +0100

    - Added two new activities for notepad feature;

---
+++ MeMe/app/src/main/java/com/spandr/meme/core/activity/notepad/NotepadDashboardActivity.java
@@ -0,0 +1,18 @@
+package com.spandr.meme.core.activity.notepad;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.spandr.meme.R;
+
+public class NotepadDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notepad_dashboard);
+    }
+
+
+
+}
---
+++ MeMe/app/src/main/java/com/spandr/meme/core/activity/notepad/category/NotepadCategoryActivity.java
@@ -0,0 +1,17 @@
+package com.spandr.meme.core.activity.notepad.category;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.spandr.meme.R;
+
+public class NotepadCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notepad_category);
+    }
+
+
+}
